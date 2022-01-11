007d4f736802e2de1f4c704ebc60b7e614f71b05
Onder DAL <onder.dal@gmail.com>
2022-01-11 13:51:11 +0300

    upgrade versions

--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.54.0"
+      version = "3.71.0"
     }
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "1.17.0"
+      version = "1.22.2"
     }
   }
 }
