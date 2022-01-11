6ed7c0d5f2d82ea7410bf31548a0a16b4260fa30
Onder DAL <onder.dal@gmail.com>
2022-01-11 14:56:09 +0300

    k8s version upgraded.

--- ekscluster.tf
+++ ekscluster.tf
@@ -1,19 +1,19 @@
 resource "rancher2_cluster" "eks_cluster" {
-  name        = "eks-cluster"
-  description = "Rancher EKS cluster"
+  name        = "innovation-cluster01"
+  description = "Rancher EKS cluster 01"
   eks_config_v2 {
     cloud_credential_id = rancher2_cloud_credential.AWS.id
     region              = var.region
-    kubernetes_version  = "1.20"
+    kubernetes_version  = "1.21.2"
     logging_types       = ["audit", "api"]
     security_groups     = [var.security_group_id]
     subnets             = [var.subnet_id_a, var.subnet_id_b, var.subnet_id_c]
     node_groups {
       name          = "node_group1"
       instance_type = "t3.medium"
-      desired_size  = 3
-      max_size      = 5
-      min_size      = 2
+      desired_size  = 5
+      max_size      = 10
+      min_size      = 3
     }
     private_access      = true
     public_access       = false
